23ecb04b323f76b6717cdb8a350128aab1527611
pravin <pravin.bala@a-cti.com>
2018-04-08 14:23:00 +0530

    Adding the customer list

--- CustomerInformation.js
+++ CustomerInformation.js
@@ -2,30 +2,30 @@ function login(){
 	var	userName = document.getElementById("emailAddress").value;
 	var password = document.getElementById("password").value;
 
-	if (!userName && !password) {
+	for (var i = 0; i < localStorage.length; i++) {
+		var _getUserDetails = JSON.parse(localStorage.getItem(localStorage.key(i)));
+		if (!userName && !password) {
 		console.log("Please enter the data in the fields");
-	} else if (userName.equals(localStorage.getItem("emailAddress")) && password.equals(localStorage.getItem("password"))) {
-		alert("Correct Username " + this.userName + " password : " + password);
-	} else {
-		alert("Wrong user and password");
+		}
+		if (userName == localStorage.getItem("emailAddress") && password == localStorage.getItem("password")) {
+			alert("Correct Username " + this.userName + " password : " + password);
+			window.location = "home.html"
+		}
 	}
 }
 
 function registerForm(){
-		var userName = document.getElementById("userName").value;
-		var emailAddress = document.getElementById("emailAddress").value;
-		var password = document.getElementById("password").value;
-		var repassword = document.getElementById("repassword").value;
-		var dateOfBirth = document.getElementById("dateOfBirth").value;
+	var userRegister = {
+		userName: document.getElementById("userName").value,
+		emailAddress: document.getElementById("emailAddress").value,
+		password: document.getElementById("password").value,
+		repassword: document.getElementById("repassword").value,
+		dateOfBirth: document.getElementById("dateOfBirth").value
+	};
 
-		console.log(userName)
-
-	if (password == repassword) {
-		localStorage.setItem('userName', userName);
-		localStorage.setItem("emailAddress", emailAddress);
-		localStorage.setItem("password", password);
-		localStorage.setItem("dateOfBirth", dateOfBirth);
-		console.log(userName + " " + emailAddress + " " + password + " " + dateOfBirth);
+	if (userRegister.password == userRegister.repassword) {
+		localStorage.setItem(userRegister.emailAddress, JSON.stringify(userRegister));
+		console.log(userRegister.userName + " " + userRegister.emailAddress + " " + userRegister.password + " " + userRegister.dateOfBirth);
 		window.location = "index.html"
 	} else {
 		console.log("The password are not same");
--- todolist.js
+++ todolist.js
@@ -1,5 +1,42 @@
-document.getElementById("UserName").innerHTML = localStorage.getItem("userName");
-
 function logout(){
-	window.location = "index.html"
+	window.location = "index.html";
+}
+
+function uniqueId() {
+	return Math.random().toString(36).substr(2, 9);
+}
+
+function saveCustomerDetails(){
+	if (typeof(window.localStorage) !== 'undefined') {
+		var customerInfo = {
+			uniqueId: uniqueId(),
+			firstName: document.getElementById("firstName").value,
+			lastName: document.getElementById("lastName").value,
+			emailAddres: document.getElementById("emailAddress").value,
+			mobileNumber: document.getElementById("mobileNumber").value,
+			address: document.getElementById("address").value,
+			city: document.getElementById("city").value,
+			zipcode: document.getElementById("zipcode").value
+		};
+		let userName = customerInfo.firstName + " " + customerInfo.lastName;
+		alert(userName);
+		localStorage.setItem(customerInfo.uniqueId, JSON.stringify(customerInfo));
+		var li = document.createElement("li");
+		let childList = document.createTextNode(userName);
+		li.appendChild(childList)
+		document.getElementById("customerDetails").appendChild(li);
+		document.getElementsByTagName("input").value = null;
+	}
+}
+
+window.onload = function(){
+	//document.getElementById("UserName").innerHTML = localStorage.getItem("userName");
+	for (var i = 0; i < localStorage.length; i++) {
+		var _getCustomerData = JSON.parse(localStorage.getItem(localStorage.key(i)));
+		var li = document.createElement("li");
+		let userName = _getCustomerData.firstName + " " + _getCustomerData.lastName;
+		let childList = document.createTextNode(userName);
+		li.appendChild(childList);
+		document.getElementById("customerDetails").appendChild(li);
+	}
 }
